fb75d3aa0e1a536a4dc3789152051277b8ce7394
Stephanie Nemeth <stephanie@hey.com>
2023-05-20 21:26:04 +0200

    update program

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let childPlay;
 
 const inLocalMode = true;
 let isRecording = false;
-
+let isPlaying = false;
 const brightness = 120;
 
 // colors are GRB!
@@ -25,8 +25,7 @@ const options = {
 const channel = ws281x(1, options);
 const colors = channel.array;
 
-colors[0] = 0x000000;
-ws281x.render();
+ws281x.reset();
 
 // update color-values
 colors[0] = 0x00ffff;
@@ -54,8 +53,7 @@ client.on("message", function (topic, payload) {
 // reassurance that the connection worked
 client.on("connect", () => {
   console.log("Connected!");
-  colors[0] = 0x000000;
-  ws281x.render();
+  ws281x.reset();
 });
 
 // prints an error message
@@ -69,10 +67,10 @@ client.subscribe(process.env.TOPIC_SUB);
 // listen for pin 5 events
 mpr121.on(5, (state) => {
   console.log(`pin 5: ${state}`);
-  if (state && !isRecording) {
+  if (state && !isRecording &&!isPlaying) {
     startRecording();
   } else {
-    if (childRecord && isRecording) {
+    if (childRecord && isRecording && !isPlaying) {
       stopRecording();
     }
   }
@@ -118,12 +116,14 @@ const startRecording = () => {
 };
 
 const playFile = async (payload) => {
+  isPlaying = true;
   const file = inLocalMode ? payload : await createWavFile(payload);
   childPlay = spawn("aplay", [`${file}`]);
   childPlay.on("exit", function (code, sig) {
     if (code !== null && sig === null) {
       console.log("done playing");
       deleteFile(file);
+      isPlaying = false;
     }
   });
   childPlay.stderr.on("data", function (data) {
@@ -135,9 +135,7 @@ const stopRecording = async () => {
   colors[0] = 0x0088ff;
   ws281x.render();
   await setTimeout(1000)
-  colors[0] = 0x000000;
-  ws281x.render();
-
+  ws281x.reset();
   console.log("stopped recording");
   childRecord.kill("SIGTERM");
 };
@@ -155,7 +153,7 @@ const createWavFile = async (bufferMsg) => {
 
 // To delete file:
 const deleteFile = async (path) => {
-  await setTimeout(2000);
+  await setTimeout(1000);
   fs.unlink(path, (err) => {
     if (err) throw err; //handle your error the way you want to;
     console.log(`${path} was deleted`); //or else the file will be deleted
